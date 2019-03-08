ba5a40ef06d22b574c9353bff99e03ded9313915
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-03-08 10:07:15 +0100

    Implemented conponentDidMount and .map() for smurfs

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,22 +1,46 @@
 import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from "react-redux";
+import { getSmurfs } from './../actions/index';
+
 import './App.css';
-/*
- to wire this component up you're going to need a few things.
- I'll let you do this part on your own. 
- Just remember, `how do I `connect` my components to redux?`
- `How do I ensure that my component links the state to props?`
- */
+
 class App extends Component {
+  componentDidMount() {
+    this.props.getSmurfs();
+  }
+
   render() {
+    console.log(this.props)
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your Redux version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h2>CURRENT POPULATION OF SMURF VILLAGE: </h2>
+        
+        {this.props.smurfs.map((smurf, idx )=> {
+          return (
+            <div key={idx} className="smurfContainer">
+              <h4>{smurf.name} ({smurf.age})</h4>
+            </div>
+          )
+        })}
+
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.smurf,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return (bindActionCreators({
+    getSmurfs
+  }, dispatch))
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
